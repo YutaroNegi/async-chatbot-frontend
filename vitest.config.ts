b717be54e46dc95dc0720818b2f46b776d2e308e
Yutaro Negi <souza_yutaro@hotmail.com>
2024-10-08 11:01:13 -0300

    chore: Add setupTests.ts and vitest.config.ts files

---
+++ vitest.config.ts
@@ -0,0 +1,13 @@
+// vitest.config.ts
+import { defineConfig } from 'vitest/config'
+import react from '@vitejs/plugin-react'
+
+export default defineConfig({
+  plugins: [react()],
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    setupFiles: './src/setupTests.ts',
+    include: ['src/**/*.{test,spec}.{js,ts,jsx,tsx}'],
+  },
+})
