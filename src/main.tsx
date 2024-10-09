c4c3b671241bbfe84cabdc11ac56cc44b0a50f6f
Yutaro Negi <souza_yutaro@hotmail.com>
2024-10-09 10:38:34 -0300

    feat: add AuthProvider to main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,18 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import { ChatProvider } from './context/ChatContext'
-import { BrowserRouter } from 'react-router-dom'
-import App from './App'
-import './index.css'
-
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { ChatProvider } from './context/ChatContext';
+import { AuthProvider } from './context/AuthContext';
+import { BrowserRouter } from 'react-router-dom';
+import App from './App';
+import './index.css';
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <BrowserRouter>
-      <ChatProvider>
-        <App />
-      </ChatProvider>
+      <AuthProvider>
+        <ChatProvider>
+          <App />
+        </ChatProvider>
+      </AuthProvider>
     </BrowserRouter>
   </React.StrictMode>
-)
+);
