747091159a34aea394d174ff403e56344aea4ef9
Yutaro Negi <souza_yutaro@hotmail.com>
2024-10-09 10:40:09 -0300

    feat: add isAuthenticated conditional rendering

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,17 @@
-import { Routes, Route } from 'react-router-dom'
-import Home from './pages/Home'
-import Login from './pages/Login'
+import { Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import Login from './pages/Login/Login';
+import { useAuth } from './context/AuthContext';
 
 function App() {
+  const { isAuthenticated } = useAuth();
+
   return (
     <Routes>
-      <Route path="/" element={<Home />} />
+      <Route path="/" element={isAuthenticated ? <Home /> : <Login />} />
       <Route path="/login" element={<Login />} />
     </Routes>
-  )
+  );
 }
 
-export default App
+export default App;
