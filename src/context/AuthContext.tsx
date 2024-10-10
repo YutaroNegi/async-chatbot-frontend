98706d547213b934c804da5699ec09798a3ff72c
Yutaro Negi <souza_yutaro@hotmail.com>
2024-10-09 22:51:10 -0300

    refactor: update AuthContext default value in createContext

--- src/context/AuthContext.tsx
+++ src/context/AuthContext.tsx
@@ -18,9 +18,12 @@ interface AuthContextType {
   logout: () => void;
 }
 
-export const AuthContext = createContext<AuthContextType | undefined>(
-  undefined
-);
+export const AuthContext = createContext<AuthContextType>({
+  isAuthenticated: false,
+  login: async () => {},
+  register: async () => {},
+  logout: () => {},
+});
 
 export const AuthProvider = ({ children }: { children: ReactNode }) => {
   const [isAuthenticated, setIsAuthenticated] = useState<boolean>(false);
